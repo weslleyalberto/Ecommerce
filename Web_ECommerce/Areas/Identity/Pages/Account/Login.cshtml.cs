83954a96307b799019684adb5f3db1094efc3684
Weslley Alberto <weslleyalberto@gmail.com>
2023-01-07 20:46:21 -0300

    Mudança relacionadas à view de login

--- Web_ECommerce/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Web_ECommerce/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -75,13 +75,14 @@ namespace Web_ECommerce.Areas.Identity.Pages.Account
             /// </summary>
             [Required]
             [DataType(DataType.Password)]
+            [Display(Name ="Senha")]
             public string Password { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar ?")]
             public bool RememberMe { get; set; }
         }
 
